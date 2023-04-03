1843ac50442fa64d0a0f161f356b4bc0268976ea
Kuthumi Pepple <kuthumipepple@gmail.com>
2023-04-04 00:24:46 +0100

    add handler function for '/hello' route

--- main.go
+++ main.go
@@ -6,9 +6,23 @@ import (
 	"net/http"
 )
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != http.MethodGet {
+		http.Error(w, "method is not supported", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w, "hello!")
+}
 func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
+	http.HandleFunc("/hello", helloHandler)
 
 	fmt.Printf("Starting server at port 8000\n")
 	log.Fatal(http.ListenAndServe(":8000", nil))
