536f2edd078e7d75ea56fe5fe574bd937084c33e
Kuthumi Pepple <kuthumipepple@gmail.com>
2023-04-03 23:56:18 +0100

    set server to listen and handle requests

--- main.go
+++ main.go
@@ -1,8 +1,15 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
+
+	fmt.Printf("Starting server at port 8000\n")
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
